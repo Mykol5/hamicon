3a71abe60062ecab789a02e2505f29acfa884e94
Mykol5 <olasehindekolawole@yahoo.com>
2023-07-20 15:13:07 +0100

    host database update

--- server/database.js
+++ server/database.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   password: 'Mykol~5555',
-  host: '192.168.42.160',
+  host: '197.211.58.47',
   port: 5433, // default PostgreSQL port is 5432
   database: 'postgres',
 });
